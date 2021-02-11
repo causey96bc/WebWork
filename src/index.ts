da1c399e69a40ad21bdb6179dde258b3b575653f
brettc <causey904.bc@gmail.com>
2021-02-10 21:05:21 -0500

    adding the user method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hi There");
+import { User } from "./models/User";
+const user = new User({
+    name: 'Brett',
+    age: 25
+});
+user.set({name: 'newname', age: 99});
+console.log(user.get('name'));
+console.log(user.get('age'));
